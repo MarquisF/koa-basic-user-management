44764fea9bff60f3b58adb190ad464d9a277fed4
marquisf <marquisfffff@gmail.com>
2018-09-14 16:38:47 +0800

    comment setup

--- src/routes/getComments.js
+++ src/routes/getComments.js
@@ -1,20 +1,10 @@
-const userController = require('../controllers/user');
+const commentController = require('../controllers/comment');
 const jwt = require('jsonwebtoken');
 
 module.exports = async ctx => {
-  const result = await userController.get(ctx.request.body);
-  if (!!result) {
-    const token = jwt.sign({ foo: 'bar' }, 'shhhhh');
-    ctx.body = {
-      success: 1,
-      msg: {
-        token
-      }
-    };
-  } else {
-    ctx.body = {
-      success: 0,
-      msg: 'Authorization Failed'
-    }
-  }
+  const result = await commentController.get(ctx.request.body);
+  ctx.body = {
+    success: 1,
+    data: result
+  };
 }
